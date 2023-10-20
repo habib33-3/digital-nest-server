c065a3e732c01f3306d92b8f37b7aef7557a1492
Habibur Rahman <habib.swi16@gmail.com>
2023-10-20 14:01:10 +0600

    delete single cart item

--- index.js
+++ index.js
@@ -86,6 +86,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete cart items
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
